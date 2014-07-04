5e3926ca19183039b30a6511f047da87deccc964
Maradona Morais <maradona.morais@hotmail.com>
2014-07-04 19:45:36 -0300

    Create db.sql
    Esse arquivo deve ser importado para o banco de dados mySQL. Antes verificar a existência de um banco de dados com o mesmo nome.

---
+++ db.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.1
+-- http://www.phpmyadmin.net
+--
+-- Máquina: 127.0.0.1
+-- Data de Criação: 05-Jul-2014 às 00:39
+-- Versão do servidor: 5.5.32
+-- versão do PHP: 5.4.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `poston`
+--
+CREATE DATABASE IF NOT EXISTS `poston` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `poston`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `post`
+--
+
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `conteudo` text NOT NULL,
+  `status` varchar(65) NOT NULL,
+  `tipo` varchar(65) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=28 ;
+
+--
+-- Extraindo dados da tabela `post`
+--
+
+INSERT INTO `post` (`id`, `conteudo`, `status`, `tipo`) VALUES
+(24, 'Estudar para a prova de filosofia, todos as questÃµes. Ver no acadÃªmico se estÃ£o lÃ¡ os exercÃ­cios.', 'fazer', 'normal'),
+(23, 'Estudar para a prova de matemÃ¡tica', 'feito', 'urgente'),
+(25, 'Verificar o email, muitooo mesmo para saber sobre o envio do Intel Galileu', 'fazer', 'normal'),
+(26, 'Acessar a internet', 'fazendo', 'urgente');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
